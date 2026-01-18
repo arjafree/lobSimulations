b3ae13f602cd3fc66ca385bc7c4dad8d2630275b
arjafree <arjafree@gmail.com>
2026-01-18 20:21:56 +0000

    ^

--- HawkesRLTrading/LSTM_alone_trainer.py
+++ HawkesRLTrading/LSTM_alone_trainer.py
@@ -63,7 +63,7 @@ kwargs={
                         {"cash": 2500,
                          "strategy": "ICRL",
                          "action_freq": 0.213,
-                         "rewardpenalty": 100,
+                         "rewardpenalty": 50,
                          "Inventory": {"INTC": 0},
                          "log_to_file": True,
                          "cashlimit": 5000000,
