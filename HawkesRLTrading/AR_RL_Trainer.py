1bf4351e1a3bbe9867a9280a41ee3389bf0f59ea
arjafree <arjafree@gmail.com>
2026-03-04 15:00:48 +0000

    turned CEM on

--- HawkesRLTrading/AR_RL_Trainer.py
+++ HawkesRLTrading/AR_RL_Trainer.py
@@ -427,7 +427,7 @@ for episode in range(80):
         if ('test' not in label) and ((checkpoint_params is None) or (episode >= 0)):
             for epoch in range(1):
                 start_time = time.time()
-                d_policy_loss, d_value_loss, d_entropy_loss, u_policy_loss, u_value_loss, u_entropy_loss = agent.train(train_logger) #, use_CEM = bool((episode+1) % 4))
+                d_policy_loss, d_value_loss, d_entropy_loss, u_policy_loss, u_value_loss, u_entropy_loss = agent.train(train_logger, use_CEM = bool((episode+1) % 4))
                 train_time = time.time() - start_time
                 # store timing on the train_logger (create list if necessary)
                 print(f"Agent.train took {train_time:.4f}s for episode {episode}")
