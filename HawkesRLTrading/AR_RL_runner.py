100f18f1e94422f72538fe9838aa56f08f967df9
arjafree <arjafree@gmail.com>
2026-04-01 12:59:07 +0100

    testing both sided

--- HawkesRLTrading/AR_RL_runner.py
+++ HawkesRLTrading/AR_RL_runner.py
@@ -9,8 +9,8 @@ import random
 
 # model_dir = '/Users/alirazajafree/researchprojects/LSTM_fRL/without_expo/testing/self_imitation/newlogs/model'
 # log_dir = '/Users/alirazajafree/researchprojects/LSTM_fRL/without_expo/testing/self_imitation/newlogs/logs/'
-log_dir = '/home/ajafree/LSTM_fRL/wout_expo/testing/self_imitation/onesided/buy/termpen/logs/'
-model_dir = '/home/ajafree/LSTM_fRL/wout_expo/testing/self_imitation/onesided/buy/termpen/model'
+log_dir = '/home/ajafree/LSTM_fRL/wout_expo/testing/self_imitation/logs/'
+model_dir = '/home/ajafree/LSTM_fRL/wout_expo/testing/self_imitation/model'
 
 start_trading_lag = 100
 twap_off_time = 400
@@ -21,9 +21,9 @@ layer_widths=100
 n_layers=3
 eta = 50
 
-label = f'test_LSTM_TWAP'
+label = f'test_bothsided'
 
-checkpoint_params = ("20260323_102001_train_termpen_buy", 56)
+checkpoint_params = ("20260323_103726_train_lstm_cem_on", 64)
 
 # with open("/Users/alirazajafree/researchprojects/otherdata/Symmetric_INTC.OQ_ParamsInferredWCutoffEyeMu_sparseInfer_2019-01-02_2019-12-31_CLSLogLin_10", 'rb') as f: # INTC.OQ_ParamsInferredWCutoff_2019-01-02_2019-03-31_poisson
 with open("/home/ajafree/researchprojects/otherdata/Symmetric_INTC.OQ_ParamsInferredWCutoffEyeMu_sparseInfer_2019-01-02_2019-12-31_CLSLogLin_10", 'rb') as f: # INTC.OQ_ParamsInferredWCutoff_2019-01-02_2019-03-31_poisson
@@ -116,7 +116,7 @@ RLagentInstance = AdversarialPPOAgent( seed=1, log_events=True, log_to_file=True
                           wake_on_MO=j["wake_on_MO"], wake_on_Spread=j["wake_on_Spread"], cashlimit=j["cashlimit"],inventorylimit=j['inventorylimit'], batch_size=512,
                           layer_widths=layer_widths, n_layers =n_layers, buffer_capacity = 100000, rewardpenalty = j["rewardpenalty"], epochs = 100, transaction_cost=1e-4, start_trading_lag = j['start_trading_lag'],
                           gae_lambda=0.5, truncation_enabled=False, action_space_config = 1, alt_state=True, enhance_state=True, include_time=True, optim_type='ADAM',entropy_coef=0, exploration_bonus = 0, hidden_activation='sigmoid', 
-                          typeNN = "LSTM", lr = 3e-4, chunk_length=64, TWAPPresent=0)#, terminal_invpenalty=5*eta)
+                          typeNN = "LSTM", lr = 3e-4, chunk_length=64, TWAPPresent=0)# terminal_invpenalty=5*eta)
 
 
 # RLagentInstance = PPOAgent( seed=1, log_events=True, log_to_file=True, strategy=j["strategy"], Inventory=j["Inventory"], cash=j["cash"], action_freq=j["action_freq"],
