c0e8db789029374f6451fe1303b9369aff2b7152
arjafree <arjafree@gmail.com>
2026-05-15 10:28:00 +0100

    ready for sell run

--- HawkesRLTrading/AR_RL_Trainer.py
+++ HawkesRLTrading/AR_RL_Trainer.py
@@ -12,19 +12,19 @@ from HawkesRLTrading.src.SimulationEntities.MetaOrderTradingAgents import TWAPGy
 import torch
 import time
 
-log_dir = '/home/ajafree/LSTM_fRL/wout_expo/bugfixes/no_running_invpen/buy/training/logs/'
-model_dir = '/home/ajafree/LSTM_fRL/wout_expo/bugfixes/no_running_invpen/buy/training/model'
+log_dir = '/home/ajafree/LSTM_fRL/wout_expo/bugfixes/no_running_invpen/sell/training/logs/'
+model_dir = '/home/ajafree/LSTM_fRL/wout_expo/bugfixes/no_running_invpen/sell/training/model'
 
 start_trading_lag = 100
 twap_off_time = 400
 
-twap_side = "buy"
+twap_side = "sell"
 #the time that the TWAP agent will kick in:
 twap_start_time = 150 + start_trading_lag
 
 twap_end_time = 300 + start_trading_lag
 
-label = 'train_no_running_invpen_buy'
+label = 'train_no_running_invpen_sell'
 layer_widths=100
 n_layers=3
 eta = 5
