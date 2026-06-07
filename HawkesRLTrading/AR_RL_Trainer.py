e71af37df4808661232f02146c4a5a0ea8b2e772
arjafree <arjafree@gmail.com>
2026-06-07 11:58:32 +0100

    sell side, gae

--- HawkesRLTrading/AR_RL_Trainer.py
+++ HawkesRLTrading/AR_RL_Trainer.py
@@ -12,8 +12,8 @@ from HawkesRLTrading.src.SimulationEntities.MetaOrderTradingAgents import TWAPGy
 import torch
 import time
 
-log_dir = '/home/ajafree/LSTM_fRL/wout_expo/no_double_quoting_bonus/gae_lambda/training/buy/logs/'
-model_dir = '/home/ajafree/LSTM_fRL/wout_expo/no_double_quoting_bonus/gae_lambda/training/buy/model'
+log_dir = '/home/ajafree/LSTM_fRL/wout_expo/no_double_quoting_bonus/gae_lambda/training/sell/logs/'
+model_dir = '/home/ajafree/LSTM_fRL/wout_expo/no_double_quoting_bonus/gae_lambda/training/sell/model'
 
 start_trading_lag = 100
 twap_off_time = 400
@@ -24,7 +24,7 @@ twap_start_time = 150 + start_trading_lag
 
 twap_end_time = 300 + start_trading_lag
 
-label = 'train_higher_gae_buy'
+label = 'train_higher_gae_sell'
 layer_widths=100
 n_layers=3
 eta = 5
@@ -277,7 +277,7 @@ for episode in range(80):
     kwargs["GymTradingAgent"][1]["Inventory"] = {"INTC": 500}
     kwargs["GymTradingAgent"][1]["cash"] = 1000000
 
-    twap_side = "buy"  # buy-only run (was: np.random.choice(["buy", "sell"]))
+    twap_side = "sell"  # sell-only run (was: np.random.choice(["buy", "sell"]))
     sides.append(twap_side)
     eps_with_buy.append(episode) if twap_side == "buy" else eps_with_sell.append(episode)
     kwargs["GymTradingAgent"][1]["start_trading_lag"] = twap_start_time
